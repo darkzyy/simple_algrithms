9d9e6e484ec5dad541599ba1ddb15a1d33e5e176
131220070-Zhou Yaoyang <diamondzyy@sina.com>
2016-03-24 16:53:55 +0800

    handling smallest k elements

---
+++ 05_leastk/leastk.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+class heap {
+    int *a;
+    heap(int k) {
+        a = new int[k];
+    }
+};
+
+void find_least_k(int a[], int k, int amt) {
+
+}
+
+int main() {
+}
